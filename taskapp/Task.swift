2125ddf4c27059b92b7f01548c8fbf00e85046c0
isseium <issei@ikmz.net>
2016-10-05 14:53:48 +0900

    標準版 taskapp の実装

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Issei Komatsu on 2016/10/05.
+//  Copyright © 2016年 Issei Komatsu. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
